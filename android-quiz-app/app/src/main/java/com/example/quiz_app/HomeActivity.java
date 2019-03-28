8ad32ac628630bef07c99d4b5ee0e0efdbd490d5
Udana Rathnayaka <rathnayakar07@gmail.com>
2019-03-28 13:43:37 +0530

    spalsh-screen-initial-commit

---
+++ android-quiz-app/app/src/main/java/com/example/quiz_app/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.quiz_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+    }
+}
