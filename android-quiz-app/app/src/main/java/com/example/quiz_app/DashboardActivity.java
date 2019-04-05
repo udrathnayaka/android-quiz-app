22a4fb165a5d2fa96c2c008c4573dcb2be2f8687
Udana Rathnayaka <rathnayakar07@gmail.com>
2019-04-05 23:24:56 +0530

    Dashboard,Splash,Home,Result Implementation

--- android-quiz-app/app/src/main/java/com/example/quiz_app/DashboardActivity.java
+++ android-quiz-app/app/src/main/java/com/example/quiz_app/DashboardActivity.java
@@ -1,15 +1,86 @@
 package com.example.quiz_app;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ImageView imageAdmin;
+    ImageView imageAboutUs;
+    ImageView imageInstructions;
+    ImageView imageExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
+
+        imageAdmin = findViewById(R.id.imgAdmin);
+        imageAboutUs = findViewById(R.id.imgAboutUs);
+        imageInstructions = findViewById(R.id.imgInstructions);
+        imageExit = findViewById(R.id.imgExit);
+
+        imageAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AdministratorActivity.class);
+                startActivity(i);
+            }
+        });
+
+        imageAboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AboutUsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        imageInstructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), InstructionsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        imageExit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        DashboardActivity.this);
+
+                alertDialogBuilder.setTitle("Do you want to exit?");
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                moveTaskToBack(true);
+                            }
+                        })
+                        .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+            }
+
+    });
+
+
     }
 }
