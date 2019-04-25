f2643cd14ae01961696b0a9567030c20c048aede
shannon99 <shannonsilva699@gmail.com>
2019-04-25 20:56:45 +0530

    admin list & question view

--- android-quiz-app/app/src/main/java/com/example/quiz_app/AdminQuestion.java
+++ android-quiz-app/app/src/main/java/com/example/quiz_app/AdminQuestion.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AdminQuestion extends AppCompatActivity {
 
-    Button btnUpdate;
+    Button btnUpdate, btnDelete;
+    TextView quest, answ, opt1, opt2, opt3, opt4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +19,46 @@ public class AdminQuestion extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_admin_question);
 
-        //load question details into the ui when row is clicked
+        //initialize btns & textviews
+        quest = (TextView) findViewById(R.id.questionn);
+        opt1 = (TextView) findViewById(R.id.onee);
+        opt2 = (TextView) findViewById(R.id.twoo);
+        opt3 = (TextView) findViewById(R.id.threee);
+        opt4 = (TextView) findViewById(R.id.fourr);
+        answ = (TextView) findViewById(R.id.answerr);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
 
-        btnUpdate = findViewById(R.id.btnUpdate);
+        //get details from list row using intent
+        Intent intent = getIntent();
+        String question = intent.getStringExtra("q");
+        String one = intent.getStringExtra("one");
+        String two = intent.getStringExtra("two");
+        String three = intent.getStringExtra("three");
+        String four = intent.getStringExtra("four");
+        String answer = intent.getStringExtra("ans");
+
+        //load question details into the ui when row is clicked
+        quest.setText(question);
+        opt1.setText(one);
+        opt2.setText(two);
+        opt3.setText(three);
+        opt4.setText(four);
+        answ.setText(answer);
 
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //redirect to update activity
                 Intent i = new Intent(getApplicationContext(),UpdateActivity.class);
                 startActivity(i);
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View view) {
+                //code to delete selected record
             }
         });
     }
--- android-quiz-app/app/src/main/java/com/example/quiz_app/AdminQuestionList.java
+++ android-quiz-app/app/src/main/java/com/example/quiz_app/AdminQuestionList.java
@@ -1,10 +1,13 @@
 package com.example.quiz_app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -13,19 +16,26 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.quiz_app.Model.Question;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.quiz_app.R.layout.question_row;
+
 public class AdminQuestionList extends AppCompatActivity {
 
     ListView listView;
     FloatingActionButton fab;
-    DatabaseReference ref;
+    DatabaseReference ref, ref1;
     //list to store questions from db
     List<Question> questions;
+    ArrayAdapter<String>arrayAdapter;
+    List<String> q;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,15 +48,50 @@ public class AdminQuestionList extends AppCompatActivity {
 
         ref= FirebaseDatabase.getInstance().getReference("questions");
         questions = new ArrayList<>();
+        q = new ArrayList<>();
+        final String[] idd = new String[1];
+
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Question value = dataSnapshot.getValue(Question.class);
+                //idd[0] = dataSnapshot.getKey();
+                //Log.e("iddddd",idd[0]);
+                questions.add(value);
+                q.add(value.getQuestion());
+                arrayAdapter = new ArrayAdapter<String>(AdminQuestionList.this, android.R.layout.simple_list_item_1, q);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-                //Toast.makeText(AdminQuestionList.this,"Clicked Item:"+1+""+arrayList.get(i).toString(),Toast.LENGTH_SHORT).show();
-
                 Question question = questions.get(i);
                 Intent i2 =new Intent(AdminQuestionList.this,AdminQuestion.class);
 
+                //i2.putExtra("id",idd);
+                //Log.e("id", String.valueOf(idd[0]));
                 i2.putExtra("q",question.getQuestion());
                 i2.putExtra("one",question.getOption1());
                 i2.putExtra("two",question.getOption2());
@@ -55,7 +100,6 @@ public class AdminQuestionList extends AppCompatActivity {
                 i2.putExtra("ans",question.getAnswer());
 
                 startActivity(i2);
-
             }
         });
 
